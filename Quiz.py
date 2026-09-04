63b63657e8058808ef90aaa9a033f47c3ac27875
skute09 <236686334+skute09@users.noreply.github.com>
2026-09-04 12:56:59 +0200

    La til kommentarer i koden og lagde readme.md

--- Quiz.py
+++ Quiz.py
@@ -1,7 +1,13 @@
 print("Velkommen til dette Quiz-spillet!")
+# Spør om hva du heter, printer det ut og definerer et globalt variabel som heter "poeng"
 navn = input("Hva heter du? ")
 print("Så kjekt at du vil være med " + navn + "!")
 poeng = 0
+
+# Har tre if statements etter hverandre som hver sjekker et nytt input variabel om hva den returner
+# sammenligner svaret til spilleren med svaret og sier i fra om du hadde rett eller galt
+# legger til et tall i poeng variablet viss du har rett
+# passer på at svaret ikke er case sensitiv ved å bruke .lower
 
 svar1 = input("Første spørsmål: Hva er hovedstaden i Norge? ")
 if svar1.lower() == "oslo":
@@ -23,6 +29,10 @@ if svar3.lower() == "brann":
     poeng +=1
 else:
     print("Beklager, men det er feil svar.")
+
+# Gratulerer deg og kaller deg navne du valgte i begynnelsen 
+# Sier hvor mye poeng du fikk
+# Gir deg en melding basert på hver mulige poeng sum
 
 print(f"Gratulerer {navn}! Du fikk {poeng} poeng!")
 if poeng == 3:
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Quiz-spill
-I dette prosjektet skal dere legge til kommentarer og en passende README som sier noe om prosjektet
+
+Dette er et quiz spill hvor du får ulike spørsmål og får poeng om du har rett.
+
+Du må ha vs code som du kan laste ned [her](https://code.visualstudio.com/).
+Når du har vs code så trykker du her og søker etter python av Microsoft og installerer den.
+
+![alt text](image.png)
+
+trykk her i høyre hjørne for å kjøre koden
+
+![alt text](image-1.png)
